a666716c916a8211edf0f426a88984df76b3c830
imwiily <ezebesen@gmail.com>
2025-08-09 09:28:15 -0300

    Added: EndPoint GET to see all subcategories of a Category

--- src/main/java/com/wiily/pscosmeticos/PsAPI/domain/category/CategoryRepository.java
+++ src/main/java/com/wiily/pscosmeticos/PsAPI/domain/category/CategoryRepository.java
@@ -3,5 +3,4 @@ package com.wiily.pscosmeticos.PsAPI.domain.category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findByNome(String nome);
 }
